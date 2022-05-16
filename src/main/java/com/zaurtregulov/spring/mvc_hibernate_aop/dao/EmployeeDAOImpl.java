4a30046a5a2ea0b99e476bca8a0166c421aa6e18
iberbasov <iberbasov@bk.ru>
2022-05-16 22:07:23 +0400

    added update, delete buttons added logging aspect for dao class

--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -29,4 +29,30 @@ public class EmployeeDAOImpl implements EmployeeDAO{
 
         return allEmployees;
     }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(employee);
+
+
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+
+        Session session = sessionFactory.getCurrentSession();
+        Employee employee = session.get(Employee.class, id);
+
+        return employee;
+    }
+
+    public void deleteEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Employee> query = session.createQuery("delete from Employee " +
+                "where id =:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
+    }
 }
--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-public class EmployeeServiceImpl implements EmployeeService{
+public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeDAO employeeDAO;
@@ -18,5 +18,24 @@ public class EmployeeServiceImpl implements EmployeeService{
     public List<Employee> getAllEmployees() {
 
         return employeeDAO.getAllEmployees();
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+
+        employeeDAO.saveEmployee(employee);
+    }
+
+    @Override
+    @Transactional
+    public Employee getEmployee(int id) {
+        return employeeDAO.getEmployee(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(int id) {
+        employeeDAO.deleteEmployee(id);
     }
 }
